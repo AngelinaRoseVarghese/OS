b181efc5cd1c4fa5e11bdd2213baa0eb11b725a9
ANGELINA ROSE VARGHESE <angelinarosevarghese2028@cs.sjcetpalai.ac.in>
2026-03-28 23:03:08 +0530

    Update producer_consumer.c

--- producer_consumer.c
+++ producer_consumer.c
@@ -1,19 +1,11 @@
 #include<stdio.h>
-
 int main()
 {
-    int mutex=1, full=0, empty=3;
-    int x=0;
-    int choice;
-
+    int mutex=1, full=0, empty=3,x=0,choice;
     while(1)
     {
-        printf("\n1. Produce");
-        printf("\n2. Consume");
-        printf("\n3. Exit");
-        printf("\nEnter your choice: ");
+        printf("\n1. Produce \n2. Consume \n3. Exit \nEnter your choice:");
         scanf("%d",&choice);
-
         if(choice==1)
         {
             if(mutex==1 && empty!=0)
@@ -22,9 +14,7 @@ int main()
                 full=full+1;
                 empty=empty-1;
                 x=x+1;
-
                 printf("Producer produced item %d",x);
-
                 mutex=1;
             }
             else
@@ -32,18 +22,15 @@ int main()
                 printf("Buffer is full");
             }
         }
-
         else if(choice==2)
         {
             if(mutex==1 && full!=0)
             {
                 mutex=0;
                 printf("Consumer consumed item %d",x);
-
                 full=full-1;
                 empty=empty+1;
                 x=x-1;
-
                 mutex=1;
             }
             else
@@ -51,7 +38,6 @@ int main()
                 printf("Buffer is empty");
             }
         }
-
         else if(choice==3)
         {
             break;
